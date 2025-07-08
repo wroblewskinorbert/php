a60a0d76584a035282572096e62d82e79833fce3
Norbert Wróblewski <wroblewskinorbert@users.noreply.github.com>
2025-07-08 20:19:21 +0000

    pierwszy php

--- index.php
+++ index.php
@@ -4,8 +4,23 @@
     
   </head>
   <body>
-    <h1>Wspaniale</h1>
-    <h2>Witaj na mojej stronie!</h2>
+<?php
+for($x=0; $x < 5; $x++){
+	echo "My first PHP script!\n<br>";
+}
+
+$age = 18;
+
+$output = match (true) {
+    $age < 2 => "Baby",
+    $age < 13 => "Child",
+    $age <= 19 => "Teenager",
+    $age >= 40 => "Old adult",
+    $age > 19 => "Young adult",
+};
+
+var_dump($output);
+?> 
     
   </body>
 </html>
