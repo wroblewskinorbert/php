5bdfbc44a33e8d9f0a4d459226eb265ebee521a0
Norbert Wróblewski <wroblewskinorbert@users.noreply.github.com>
2025-07-14 14:53:45 +0000

    dodałem formularz

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+
+
+?>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Próba z formularzem</title>
+</head>
+<body>
+    <h1>Please submit the form</h1>
+    <form action="/" method="POST">
+        <label>Email:</label>
+        <input type="email" name="email" />
+        <button>Submit<botton/>
+    </form>
+</body>
+</html>
